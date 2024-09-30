f9d76304a0800327e0f9ac37acb6a40490ff754f
Marek Tomasz Miśkowiec <miskowiec.ma@gmail.com>
2024-09-30 16:51:56 +0200

    fix images extension and created champion and runes tier list

--- src/components/ChampionFilter.jsx
+++ src/components/ChampionFilter.jsx
@@ -1,40 +1,44 @@
-import { useState } from 'react';
-import './ChampionFilter.css'
+import { useState } from "react";
+import "./ChampionFilter.css";
 
-const ChampionFilter = ({ champions }) => {
-  const roles = [/*'all',*/ 'baron', 'jungle', 'mid', 'dragon', 'support'];
-  
-  const [selectedRole, setSelectedRole] = useState('baron');
+const ChampionFilter = ({ champions, folder, filter_type }) => {
+  const roles = filter_type;
+
+  const [selectedRole, setSelectedRole] = useState(filter_type[0]);
 
   const filterChampionsByRole = (tierChampions, role) => {
-    // if (role === 'all') return tierChampions;
-    return tierChampions.filter(champion => champion.roles.includes(role));
+    if (role === "all") return tierChampions;
+    return tierChampions.filter((champion) => champion.roles.includes(role));
   };
 
   return (
     <div>
       <div className="role-buttons">
-        {roles.map(role => (
-          <button 
+        {roles.map((role) => (
+          <button
             key={role}
-            className={`btn ${selectedRole === role ? 'active' : ''}`}
-            onClick={() => setSelectedRole(role)}>
+            className={`btn ${selectedRole === role ? "active" : ""}`}
+            onClick={() => setSelectedRole(role)}
+          >
             {role}
           </button>
         ))}
       </div>
 
-      {Object.keys(champions).map(tier => (
+      {Object.keys(champions).map((tier) => (
         <div className="container" key={tier}>
-          <div className="box1">{tier.replace('_', ' ').toUpperCase()}</div>
-          <div className="box2">
-            {
-              filterChampionsByRole(champions[tier], selectedRole).map(champion => (
+          <div className="tier-box">{tier.replace("_", " ").toUpperCase()}</div>
+          <div className="champion-box">
+            {filterChampionsByRole(champions[tier], selectedRole).map(
+              (champion) => (
                 <div className="box" key={champion.name}>
-                  <img src={`/champions/${champion.name}.png`} alt={champion.name} />
+                  <img
+                    src={`/${folder}/${champion.name}.webp`}
+                    alt={champion.name}
+                  />
                 </div>
-              ))
-            }
+              )
+            )}
           </div>
         </div>
       ))}
