c7d85c0c4cf6b88bbde8dc995cf80d7e6538a136
Marek Tomasz Miśkowiec <miskowiec.ma@gmail.com>
2024-10-10 20:44:46 +0200

    added pagination

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const SITE_TITLE = "Wildrift Website";
 export const SITE_DESCRIPTION = "Builds, Guides, Articles about wildrift";
 export const CURRENT_PATCH = "PATCH 5.2d";
+
+export const ARTICLES_PER_PAGE = 6;
