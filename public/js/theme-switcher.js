c632923da193e790fe3daa17e9daa951ac496542
Marek Tomasz Miśkowiec <miskowiec.ma@gmail.com>
2024-10-11 11:35:19 +0200

    added dark theme

---
+++ public/js/theme-switcher.js
@@ -0,0 +1,10 @@
+let icon = document.getElementById("icon");
+
+icon.onclick = () => {
+  document.body.classList.toggle("light-theme");
+  //   if (document.body.classList.contains("light-theme")) {
+  //     icon.src = "./assets/images/moon.png";
+  //   } else {
+  //     icon.src = "./assets/images/sun.png";
+  //   }
+};
--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE = "Wildrift Website";
 export const SITE_DESCRIPTION = "Builds, Guides, Articles about wildrift";
 export const CURRENT_PATCH = "PATCH 5.2d";
 
-export const ARTICLES_PER_PAGE = 1;
+export const ARTICLES_PER_PAGE = 6;
